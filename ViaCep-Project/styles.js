5201a56a9f39d0757cf1c82bcf92428f05195458
kamille <kamille.senai@gmail.com>
2024-02-02 13:14:35 -0300

    acresentado /native no style boxinput

--- ViaCep-Project/styles.js
+++ ViaCep-Project/styles.js
@@ -1,7 +1,7 @@
 //estilizações da raiz do projeto
 //faz referencia ao app.js
 
-import styled from "styled-components";
+import styled from "styled-components/native";
 
 //componente container app
 export const ContainerApp= styled.View`
